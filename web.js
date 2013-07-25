ed4245e4eff8d650ff2ae7895265784f9ead0830
Riko Huang <ric-ko-92@hotmail.com>
2013-07-25 11:34:53 +0000

    changed web.js

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
+#!/usr/bin/env node
+
 var express = require('express');
 var fs = require('fs');
 var app = express();
 app.use(express.logger());
 app.get('/', function(request, response) {
-  fs.readFileSync('index.html');
-  response.send('index.html');
+  var file = fs.readFileSync('index.html', 'utf-8');
+  response.send(file);
 });
 
 var port = process.env.PORT || 5000;
